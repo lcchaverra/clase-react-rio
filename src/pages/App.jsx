21a40e7e709ffb5bcf99ca061db31b45f889560a
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-08-22 16:11:40 -0500

    primer commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+    <div className="presentation">
+      <h2>PRIMERA APP EN REACT</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, exercitationem asperiores eveniet neque explicabo unde odio soluta autem doloribus. Vitae ea minus consequatur magni enim aliquam fugiat neque distinctio suscipit.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
